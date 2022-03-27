8c4219c7f54906c2ed14eebf75aed985feb09e7b
sophiajay <45126574+sophiajay@users.noreply.github.com>
2022-03-27 11:41:25 -0700

    fixed makefile, added parts.c

--- structure.c
+++ structure.c
@@ -7,16 +7,19 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <time.h>
+#include <arpa/inet.h>
 
 struct __attribute__((__packed__)) superblock_t {
-    uint8_t   fs_id [8];
-    uint16_t block_size;
-    uint32_t file_system_block_count;
-    uint32_t fat_start_block;
-    uint32_t fat_block_count;
-    uint32_t root_dir_start_block;
-    uint32_t root_dir_block_count;
+    uint8_t   fs_id [8];  //8 bytes
+    uint16_t block_size;  //2
+    uint32_t file_system_block_count;  //4
+    uint32_t fat_start_block;  //4
+    uint32_t fat_block_count;  //4
+    uint32_t root_dir_start_block; //4
+    uint32_t root_dir_block_count;  //4
 };
+
+
 
 void main(int argc, char* argv[]) {
 
